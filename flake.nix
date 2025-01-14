f643e064763e07ef8b9e0ecce8757b8af9b3277b
Jeremy <yiliang.he@qq.com>
2025-01-14 10:39:34 +0800

    Add espresso verification (#61)
    * Add verification

--- flake.nix
+++ flake.nix
@@ -1,18 +1,19 @@
 {
   description = "Golang SDK for the Espresso Sequencer";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
-
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.foundry.url = "github:shazow/foundry.nix/monthly";
 
-  outputs = { self, flake-utils, nixpkgs, foundry, ... }:
+  outputs = { flake-utils, nixpkgs, foundry, rust-overlay, ... }:
     let
-      goVersion = 21;
+      goVersion = 22; # Change this to update the whole stack
       overlays = [
-        (final: prev: {
+        (import rust-overlay)
+        (final: prev: rec {
           go = prev."go_1_${toString goVersion}";
         })
         foundry.overlay
@@ -23,30 +24,109 @@
         pkgs = import nixpkgs {
           inherit overlays system;
         };
-        # nixWithFlakes allows pre v2.4 nix installations to use flake commands (like
-        # `nix flake update`)
-        nixWithFlakes = pkgs.writeShellScriptBin "nix" ''
-          exec ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
+        stableToolchain = pkgs.rust-bin.stable."1.81.0".minimal.override {
+          extensions = [ "rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" "wasm32-wasi" ];
+        };
+        nightlyToolchain = pkgs.rust-bin.nightly."2024-08-06".minimal.override {
+          extensions = [ "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" "wasm32-wasi" ];
+        };
+        # A script that calls nightly cargo if invoked with `+nightly`
+        # as the first argument, otherwise it calls stable cargo.
+        cargo-with-nightly = pkgs.writeShellScriptBin "cargo" ''
+          if [[ "$1" == "+nightly" ]]; then
+            shift
+            # Prepend nightly toolchain directory containing cargo, rustc, etc.
+            exec env PATH="${nightlyToolchain}/bin:$PATH" cargo "$@"
+          fi
+          exec ${stableToolchain}/bin/cargo "$@"
+        '';
+        shellHook = ''
+          # Prevent cargo aliases from using programs in `~/.cargo` to avoid conflicts
+          # with rustup installations.
+          export CARGO_HOME=$HOME/.cargo-nix
+          export DOCKER_BUILDKIT=1
+
+          # Create a target directory and ensure lib64 is a symlink to lib.
+          # Individual build steps may target either directory and later
+          # create the symlink making some build outputs inaccessible.
+          mkdir -p target/lib
+          ln -sf lib target/lib64
+        ''
+        + pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
+          # Fix docker-buildx command on OSX. Can we do this in a cleaner way?
+          mkdir -p ~/.docker/cli-plugins
+          # Check if the file exists, otherwise symlink
+          test -f $HOME/.docker/cli-plugins/docker-buildx || ln -sn $(which docker-buildx) $HOME/.docker/cli-plugins
         '';
       in
       {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            nixWithFlakes
-            go
-            # goimports, godoc, etc.
-            gotools
-            # https://github.com/golangci/golangci-lint
-            golangci-lint
-            jq
-            just
-            foundry-bin
-            go-ethereum
-          ] ++ lib.optionals stdenv.isDarwin [
-            darwin.libobjc
-            darwin.IOKit
-            darwin.apple_sdk.frameworks.CoreFoundation
-          ];
-        };
+        devShells =
+          {
+            # mkShell brings in a `cc` that points to gcc, stdenv.mkDerivation from llvm avoids this.
+            default = let llvmPkgs = pkgs.llvmPackages_16; in llvmPkgs.stdenv.mkDerivation {
+              hardeningDisable = [
+                # By default stack protection is enabled by the clang wrapper but I
+                # think it's not supported for wasm compilation. It causes this
+                # error:
+                #
+                #   Undefined stack protector symbols: __stack_chk_guard ...
+                #   in arbitrator/wasm-libraries/soft-float/SoftFloat/build/Wasm-Clang/extF80_div.o
+                "stackprotector"
+                # See https://github.com/NixOS/nixpkgs/pull/256956#issuecomment-2351143479
+                "zerocallusedregs"
+              ];
+
+              name = "espresso-sequencer-dev-shell";
+              buildInputs = with pkgs; [
+                cmake
+                cargo-with-nightly
+                stableToolchain
+                openssl
+                pkg-config
+
+                llvmPkgs.clang
+                llvmPkgs.bintools # provides wasm-ld
+
+                go
+                # goimports, godoc, etc.
+                gotools
+                golangci-lint
+                gotestsum
+
+                # wasm
+                rust-cbindgen
+                wabt
+
+                jq
+                just
+
+                # provides abigen
+                go-ethereum
+              ] ++ lib.optionals stdenv.isDarwin [
+                apple-sdk_11
+              ] ++ lib.optionals (! stdenv.isDarwin) [
+                glibc_multi.dev # provides gnu/stubs-32.h
+              ];
+              shellHook = shellHook + ''
+                export LIBCLANG_PATH="${pkgs.llvmPackages_16.libclang.lib}/lib"
+                export CC="${pkgs.clang-tools_16.clang}/bin/clang"
+                export AR="${pkgs.llvm_16}/bin/llvm-ar"
+              ''
+                # The clang wrapper cannot find SystemConfiguration symbols on darwin
+                # Undefined symbols for architecture arm64: "_SCDynamicStoreCopyProxies", referenced from:
+                # system_configuration::dynamic_store::SCDynamicStore::get_proxies::h29c4032f420db6e7
+                # in libespresso_crypto_helper.a(system_configuration-0133a45c6b0a8ed2.system_configuration.3319bd173d7021d9-cgu.0.rcgu.o)
+                #
+                # TODO: I think this shouldn't be required. We should probably
+                # re-think how this flake works on darwin after the changes in
+                # https://github.com/NixOS/nixpkgs/pull/346043
+                + pkgs.lib.optionalString pkgs.stdenv.isDarwin
+                ''
+                  export NIX_LDFLAGS="-framework SystemConfiguration $NIX_LDFLAGS"
+                '';
+            };
+          };
       });
 }
--- justfile
+++ justfile
@@ -10,3 +10,28 @@ bind-light-client:
 	cd espresso-sequencer/contracts/out/LightClientMock.sol && cat LightClientMock.json | jq .abi > LightClientMock.abi
 	abigen --abi espresso-sequencer/contracts/out/LightClient.sol/LightClient.abi --pkg lightclient --out light-client/lightclient.go
 	abigen --abi espresso-sequencer/contracts/out/LightClientMock.sol/LightClientMock.abi --pkg lightclientmock --out light-client-mock/lightclient.go
+
+verification_dir := "./verification/rust"
+target_lib := "./target/lib"
+
+triple := if arch() == "aarch64" {
+	if os() == "macos" {
+		"aarch64-apple-darwin"
+	} else {
+		"aarch64-unknown-linux-gnu"
+	}
+} else if arch() == "x86_64" {
+	if os() == "macos" {
+		"x86_64-apple-darwin"
+	} else {
+		"x86_64-unknown-linux-gnu"
+	}
+} else {
+	error("{{arch()}} is not supported")
+}
+
+build-verification:
+	mkdir -p {{target_lib}}
+	cargo build --release --manifest-path {{verification_dir}}/Cargo.toml
+	install {{verification_dir}}/target/release/libespresso_crypto_helper.a {{target_lib}}/libespresso_crypto_helper-{{triple}}.a
+	go build ./verification
